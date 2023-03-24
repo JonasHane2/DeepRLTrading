9396e7da2a017510c37fbce1537cbf915dbcc769
Plon <plon_96@hotmail.com>
2023-03-25 00:32:50 +0100

    conv critic channels

--- networks.py
+++ networks.py
@@ -247,41 +247,50 @@ class LinearDiscrete(nn.Module):
 # ------------------ State-Action
 ## ----------------- Conv
 class CConvSA(nn.Module):
-    def __init__(self, observation_space=8, hidden_size=64, action_space=1, dropout=0.1, kaiming_init=False) -> None:
-        super(CConvSA, self).__init__()         
-        self.prev_action_size = action_space
+    def __init__(self, observation_space=8, hidden_size=64, action_space=1, dropout=0.1, kaiming_init=False, prev_action_size=None, in_channels=1, flattened_state=False, kernel_size_1=5, stride_1=3, kernel_size_2=3, stride_2=1, kernel_size_pooling=2, stride_pooling=1) -> None:
+        super(CConvSA, self).__init__()   
+        if prev_action_size is not None:
+            self.prev_action_size = prev_action_size
+        else: 
+            self.prev_action_size = action_space
         self.fc1 = nn.Linear(observation_space,hidden_size*2)
         self.fc2 = nn.Linear(((hidden_size*2)+action_space),hidden_size)
         self.conv = nn.Sequential(
-            nn.Conv1d(1, hidden_size, kernel_size=4, stride=2), 
+            nn.Conv1d(in_channels, hidden_size, kernel_size=kernel_size_1, stride=stride_1), 
             nn_activation_function, 
-            nn.BatchNorm1d(hidden_size), 
+            nn.BatchNorm1d(hidden_size), #seems like batch norm works better after activation than before
             nn.Dropout(p=dropout), 
-            nn.Conv1d(hidden_size, hidden_size, kernel_size=3, stride=1), 
-            nn.MaxPool1d(kernel_size=2, stride=2), 
+            nn.Conv1d(hidden_size, hidden_size, kernel_size=kernel_size_2, stride=stride_2), 
+            nn.MaxPool1d(kernel_size=kernel_size_pooling, stride=stride_pooling), 
             nn_activation_function,
             nn.BatchNorm1d(hidden_size),
             nn.Dropout(p=dropout), 
         )
-        conv_out = _get_seq_out(self.conv, hidden_size)
-        self.fc3 = nn.Linear((conv_out+self.prev_action_size),1)
+        conv_out = _get_seq_out(self.conv, hidden_size, in_channels)
+        self.fc_out = nn.Linear((conv_out+self.prev_action_size), action_space)
         if kaiming_init:
             self.apply(weights_init)
-        self.fc3.weight.data.normal_(0, out_layer_std)
+        self.fc_out.weight.data.normal_(0, out_layer_std)
+        self.flattened_state = flattened_state
         
     def forward(self, state, action, prev_action=None):
         x = self.fc1(state)
         x = nn_activation_function(x)
-        x = torch.cat((x, action), dim=1).to(device)
+        if self.flattened_state:
+            action = action.unsqueeze(-1).repeat(1, x.shape[1], 1)
+            x = torch.cat((x, action), dim=2).to(device)
+        else:
+            x = torch.cat((x, action), dim=1).to(device)
         x = self.fc2(x)
         x = nn_activation_function(x)
-        x = x.unsqueeze(1) 
+        if len(x.shape) < 3:
+            x = x.unsqueeze(1) 
         x = self.conv(x)
         x = x.view(x.shape[0], -1)
         if prev_action is None: 
             prev_action = torch.Tensor(np.zeros((x.shape[0],self.prev_action_size))).to(device)         
         x = torch.cat((x, prev_action.to(device)), dim=1).to(device)
-        x = self.fc3(x)
+        x = self.fc_out(x)
         return x
 
 
